e4cffbd77ab1f111bf5ce5b23227ea22b1b77daf
Matthew Wette <mwette@alumni.caltech.edu>
2024-11-15 13:07:47 -0800

    update for nyacc 2.0

--- guix.scm
+++ guix.scm
@@ -1,6 +1,6 @@
 (define-public guile-libyaml
   (let ((commit "f5d33a6880e96571d3cb079ed7755ffc156cac46")
-        (revision "1"))
+        (revision "2"))
     (package
      (name "guile-libyaml")
      (version (git-version "0" revision commit))
@@ -29,39 +29,19 @@
 	    (lambda* (#:key inputs #:allow-other-keys)
 	      (for-each
 	       delete-file
-	       '("guix.scm" "demo1.yml" "demo1.scm"
-		 "yaml/libyaml.scm"
-		 ;; This file is mismatched with the generated FFI code.
-		 "yaml/ffi-help-rt.scm"))
-	      (copy-file
-	       (string-append
-		(assoc-ref inputs "nyacc")
-		"/share/guile/site/3.0/system/ffi-help-rt.scm")
-	       "yaml/ffi-help-rt.scm")
-               (substitute* "yaml/ffi-help-rt.scm"
-                 (("system ffi-help-rt") "yaml ffi-help-rt"))
+	       '("guix.scm" "demo1.yml" "demo1.scm"))
                #true))
            (add-before 'build 'build-ffi
              (lambda* (#:key inputs #:allow-other-keys)
-               (invoke "guild" "compile-ffi"
-                       "--no-exec" ; allow us to patch the generated file
-                       "yaml/libyaml.ffi")
-               (substitute* "yaml/libyaml.scm"
-                 (("system ffi-help-rt") "yaml ffi-help-rt")
-                 (("dynamic-link \"libyaml\"")
-                  (format #false "dynamic-link \"~a/lib/libyaml\""
-                          (assoc-ref inputs "libyaml"))))
+               (invoke "guild" "compile-ffi" "yaml/libyaml.ffi")
                #true))
            (replace 'build
              (assoc-ref guile:%standard-phases 'build))
            (delete 'install))))
       (inputs
        `(("guile" ,guile-3.0)
+         ("nyacc" ,nyacc)
          ("libyaml" ,libyaml)))
-      (propagated-inputs
-       `(("guile-bytestructures" ,guile-bytestructures)))
-      (native-inputs
-       `(("nyacc" ,nyacc)))
       (home-page "https://github.com/mwette/guile-libyaml")
       (synopsis "Guile wrapper for libyaml")
       (description
--- yaml.scm
+++ yaml.scm
@@ -1,6 +1,6 @@
 ;; yaml.scm - yaml
 
-;; Copyright (C) 2020-2021 Matthew R. Wette
+;; Copyright (C) 2020-2021,2024 Matthew Wette
 ;;
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
@@ -20,9 +20,10 @@
 (define-module (yaml)
   #:export (read-yaml-file cnvt-tree)
   #:use-module (yaml libyaml)
-  #:use-module (system ffi-help-rt)
-  #:use-module (bytestructures guile)
-  #:use-module ((system foreign) #:prefix ffi:))
+  #:use-module (nyacc foreign cdata)
+  #:use-module ((system foreign) #:prefix ffi:)
+  #:use-module (system foreign-library)
+  #:version (2 0 0))
 
 (use-modules (ice-9 format))
 (define (ff fmt . args) (apply format #t fmt args))
@@ -30,24 +31,26 @@
 (use-modules (ice-9 pretty-print))
 (define pp pretty-print)
 
-(cond-expand
- (guile-3
-  (use-modules (ice-9 exceptions))
-  (define (file-not-found filename)
-    (raise-exception
-     (make-exception-with-message
-      (string-append
-       "read-yaml-file: can't access file: " filename)))))
- (else
-  (define (file-not-found filename)
-    (scm-error 'misc "read-yaml-file"
-	       "file not found: ~S" (list filename) #f))))
+(use-modules (ice-9 exceptions))
+(define (file-not-found filename)
+  (raise-exception
+   (make-exception-with-message
+    (string-append
+     "read-yaml-file: can't access file: " filename))))
 
-(define-syntax-rule (bs-ref obj ...)
-  (bytestructure-ref obj ...))
+(define fopen
+  (let ((fopen (foreign-library-function
+                #f "fopen" #:return-type '* #:arg-types '(* *))))
+    (lambda (path mode)
+      (let ((path (ffi:string->pointer path))
+            (mode (ffi:string->pointer mode)))
+        (fopen path mode)))))
 
-(define-syntax-rule (fh-ref obj ...)
-  (fh-object-ref obj ...))
+(define fclose
+  (foreign-library-function
+   #f "fclose" #:return-type ffi:int #:arg-types '(*)))
+
+(define int*-type (cpointer (cbase 'int)))
 
 ;; works w/ bytestructures
 ;; tag-property created via (make-object-property) used to track tags
@@ -60,67 +63,71 @@
       (lambda (node tag)
 	(if (and tag-property
 		 (not (equal? tag "tag:yaml.org,2002:str")))
-	    ;;(unless (hash-ref dict tag) (hash-set! dict tag tag))
 	    (set! (tag-property node) (hash-ref dict tag)))
 	node)))
 
   (define (cnvt-scalar-node node)
     (let* ((style (wrap-yaml_scalar_style_t
-		   (bs-ref node 'data 'scalar 'style)))
-	   (raw (bytestructure-ref node 'data 'scalar 'value))
-	   (val (ffi:pointer->string (ffi:make-pointer raw))))
+                   (cdata*-ref node 'data 'scalar 'style)))
+	   (raw (cdata*-ref node 'data 'scalar 'value))
+	   (val (ffi:pointer->string raw)))
       val))
 
   (define (cnvt-sequence-node node)
     (let* ((style (wrap-yaml_sequence_style_t
-		   (bs-ref node 'data 'sequence 'style)))
-	   (start (bs-ref node 'data 'sequence 'items 'start))
-	   (end (bs-ref node 'data 'sequence 'items 'end))
-	   (top (bs-ref node 'data 'sequence 'items 'top))
-	   (item-size (bytestructure-descriptor-size yaml_node_item_t-desc)))
-      (let loop ((sequence '()) (addr (- top item-size)))
-	(if (>= addr start)
-	    (let* ((item (bytestructure int*-desc addr))
-		   (indx (1- (bs-ref item '*)))
-		   (nd (bs-ref stack indx)))
-	      (loop (cons (cnvt-node nd) sequence) (- addr item-size)))
+                   (cdata*-ref node 'data 'sequence 'style)))
+	   (start-addr (ffi:pointer-address
+                        (cdata*-ref node 'data 'sequence 'items 'start)))
+	   (end-addr (ffi:pointer-address
+                      (cdata*-ref node 'data 'sequence 'items 'end)))
+	   (top-addr (ffi:pointer-address
+                      (cdata*-ref node 'data 'sequence 'items 'top)))
+	   (item-size (ctype-size yaml_node_item_t)))
+      (let loop ((sequence '()) (addr (- top-addr item-size)))
+	(if (>= addr start-addr)
+	    (let* ((item (make-cdata int*-type addr))
+		   (indx (1- (cdata*-ref item))) ; ???
+		   (nd (cdata-sel stack indx)))
+	      (loop (cons (cnvt-node (cdata& nd)) sequence) (- addr item-size)))
 	    (list->vector sequence)))))
 
   (define (cnvt-mapping-node node)
-    (let* ((style (wrap-yaml_mapping_style_t
-		   (bs-ref node 'data 'mapping 'style)))
-	   (start (bs-ref node 'data 'mapping 'pairs 'start))
-	   (end (bs-ref node 'data 'mapping 'pairs 'end))
-	   (top (bs-ref node 'data 'mapping 'pairs 'top))
-	   (pair-size (bytestructure-descriptor-size yaml_node_pair_t-desc)))
-      (let loop ((mapping '()) (addr (- top pair-size)))
-	(if (>= addr start)
-	    (let* ((pair (bytestructure yaml_node_pair_t*-desc addr))
-		   (key-ix (1- (bs-ref pair '* 'key)))
-		   (key-nd (bs-ref stack key-ix))
-		   (val-ix (1- (bs-ref pair '* 'value)))
-		   (val-nd (bs-ref stack val-ix)))
+    (let* ((style (cdata*-ref node 'data 'mapping 'style))
+	   (start-addr (ffi:pointer-address
+                        (cdata*-ref node 'data 'mapping 'pairs 'start)))
+	   (end-addr (ffi:pointer-address
+                      (cdata*-ref node 'data 'mapping 'pairs 'end)))
+	   (top-addr (ffi:pointer-address
+                      (cdata*-ref node 'data 'mapping 'pairs 'top)))
+	   (pair-size (ctype-size yaml_node_pair_t)))
+      (let loop ((mapping '()) (addr (- top-addr pair-size)))
+	(if (>= addr start-addr)
+	    (let* ((pair (make-cdata yaml_node_pair_t* addr))
+		   (key-ix (1- (cdata-ref pair '* 'key)))
+		   (key-nd (cdata& (cdata-sel stack key-ix)))
+		   (val-ix (1- (cdata-ref pair '* 'value)))
+		   (val-nd (cdata& (cdata-sel stack val-ix))))
 	      (loop (acons (cnvt-node key-nd) (cnvt-node val-nd) mapping)
 		    (- addr pair-size)))
 	    mapping))))
 
   (define (cnvt-node node)
-    (let ((type (wrap-yaml_node_type_t (bs-ref node 'type))))
+    (let ((type (cdata*-ref node 'type)))
       (add-node-tag
        (case type
 	 ((YAML_SCALAR_NODE) (cnvt-scalar-node node))
 	 ((YAML_SEQUENCE_NODE) (cnvt-sequence-node node))
 	 ((YAML_MAPPING_NODE) (cnvt-mapping-node node))
 	 (else (error "missed type" type)))
-       (ffi:pointer->string (ffi:make-pointer (bs-ref node 'tag))))))
+       (ffi:pointer->string (cdata*-ref node 'tag)))))
 
   (cnvt-node root))
 
 (define (read-yaml-file filename)
-  (let* ((parser (make-yaml_parser_t))
-	 (&parser (pointer-to parser))
-	 (document (make-yaml_document_t))
-	 (&document (pointer-to document))
+  (let* ((parser (make-cdata yaml_parser_t))
+	 (&parser (cdata&-ref parser))
+	 (document (make-cdata yaml_document_t))
+	 (&document (cdata&-ref document))
 	 (file (if (access? filename R_OK)
 		   (fopen filename "r")
 		   (file-not-found filename))))
@@ -129,9 +136,9 @@
     (yaml_parser_set_input_file &parser file)
     (yaml_parser_load &parser &document)
     
-    (let* ((start (fh-object-ref document 'nodes 'start))
-	   (stack (bytestructure yaml_node_t*-desc start))
-	   (root (fh-object-val (yaml_document_get_root_node &document)))
+    (let* ((start (cdata-ref document 'nodes 'start))
+	   (stack (make-cdata yaml_node_t* start))
+	   (root (cdata-sel (yaml_document_get_root_node &document)))
 	   (yaml (cnvt-tree root stack)))
       (yaml_document_delete &document)
       (yaml_parser_delete &parser)

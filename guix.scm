3a2b90ef60b62f4264d17251823ddd11a93e7d40
Matthew Wette <mwette@alumni.caltech.edu>
2024-12-22 08:59:42 -0800

    not sure how to write guix.scm

--- guix.scm
+++ guix.scm
@@ -1,5 +1,5 @@
-(define-public guile-libyaml
-  (let ((commit "f5d33a6880e96571d3cb079ed7755ffc156cac46")
+(define-public guile-libyaml-next
+  (let ((commit "TBD")
         (revision "2"))
     (package
      (name "guile-libyaml")
@@ -13,7 +13,7 @@
        (file-name (git-file-name name version))
        (sha256
 	(base32
-	 "12x91983fh1j39zy7kbk19acc1rqdh8515ddx1mh7l26j04k9wgq"))))
+	 "TBD"))))
      (build-system gnu-build-system)
      (arguments
       `(#:modules (((guix build guile-build-system)
@@ -30,18 +30,30 @@
 	      (for-each
 	       delete-file
 	       '("guix.scm" "demo1.yml" "demo1.scm"))
-               #true))
+              (mkdir-p "nyacc/foreign") ;; ???
+              (copy-file
+               (string-append
+                (assoc-ref inputs "nyacc")
+                "/share/guile/site/3.0/nyacc/foreign/arch-info.scm")
+               "nyacc/foreign/arch-info.scm")
+              (copy-file
+               (string-append
+                (assoc-ref inputs "nyacc")
+                "/share/guile/site/3.0/nyacc/foreign/cdata.scm")
+               "nyacc/foreign/cdata.scm")
+              #true))
            (add-before 'build 'build-ffi
              (lambda* (#:key inputs #:allow-other-keys)
-               (invoke "guild" "compile-ffi" "yaml/libyaml.ffi")
+               (invoke "guild" "compile-ffi" "--no-exec" "yaml/libyaml.ffi")
                #true))
            (replace 'build
              (assoc-ref guile:%standard-phases 'build))
            (delete 'install))))
       (inputs
        `(("guile" ,guile-3.0)
-         ("nyacc" ,nyacc)
          ("libyaml" ,libyaml)))
+      (native-inputs
+       `(("nyacc" ,nyacc-next)))
       (home-page "https://github.com/mwette/guile-libyaml")
       (synopsis "Guile wrapper for libyaml")
       (description

c599f0f48e54bdc1a5353e51d6e9e35f94afb472
Matthew Wette <mwette@alumni.caltech.edu>
2025-08-26 17:56:19 -0700

    seems to be working; no external dep's

--- yaml.scm
+++ yaml.scm
@@ -1,6 +1,6 @@
 ;; yaml.scm - yaml
 
-;; Copyright (C) 2020-2021 Matthew R. Wette
+;; Copyright (C) 2020-2021,2025 Matthew Wette
 ;;
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
@@ -20,15 +20,29 @@
 (define-module (yaml)
   #:export (read-yaml-file cnvt-tree)
   #:use-module (yaml libyaml)
-  #:use-module (system ffi-help-rt)
-  #:use-module (bytestructures guile)
+  #:use-module (yaml cdata)
   #:use-module ((system foreign) #:prefix ffi:))
 
-(use-modules (ice-9 format))
-(define (ff fmt . args) (apply format #t fmt args))
-(define (sf fmt . args) (apply simple-format #t fmt args))
-(use-modules (ice-9 pretty-print))
-(define pp pretty-print)
+(define ptr-addr ffi:pointer-address)
+(define-once cdata&-sel
+  (lambda (data . tags)
+    (let* ((data (apply cdata-sel data tags))
+           (bptr (ffi:bytevector->pointer (cdata-bv data)))
+           (addr (+ (ffi:pointer-address bptr) (cdata-ix data)))
+           (type (name-ctype 'yaml_node_t* (cpointer (cdata-ct data)))))
+      (make-cdata type addr))))
+
+(define fopen
+  (let ((~fopen (ffi:pointer->procedure
+                 '* (dynamic-func "fopen" (dynamic-link)) (list '* '*))))
+    (lambda (filename mode)
+      (~fopen (ffi:string->pointer filename) (ffi:string->pointer mode)))))
+
+(define fclose
+  (let ((~fclose (ffi:pointer->procedure
+                 ffi:int (dynamic-func "fclose" (dynamic-link)) (list '*))))
+    (lambda (file)
+      (~fclose file))))
 
 (cond-expand
  (guile-3
@@ -41,15 +55,8 @@
  (else
   (define (file-not-found filename)
     (scm-error 'misc "read-yaml-file"
-	       "file not found: ~S" (list filename) #f))))
+               "file not found: ~S" (list filename) #f))))
 
-(define-syntax-rule (bs-ref obj ...)
-  (bytestructure-ref obj ...))
-
-(define-syntax-rule (fh-ref obj ...)
-  (fh-object-ref obj ...))
-
-;; works w/ bytestructures
 ;; tag-property created via (make-object-property) used to track tags
 (define* (cnvt-tree root stack #:optional tag-property)
 
@@ -58,81 +65,78 @@
   (define add-node-tagx
     (let ((dict (make-hash-table 31)))
       (lambda (node tag)
-	(if (and tag-property
-		 (not (equal? tag "tag:yaml.org,2002:str")))
-	    ;;(unless (hash-ref dict tag) (hash-set! dict tag tag))
-	    (set! (tag-property node) (hash-ref dict tag)))
-	node)))
+        (if (and tag-property
+                 (not (equal? tag "tag:yaml.org,2002:str")))
+            ;;(unless (hash-ref dict tag) (hash-set! dict tag tag))
+            (set! (tag-property node) (hash-ref dict tag)))
+        node)))
 
   (define (cnvt-scalar-node node)
-    (let* ((style (wrap-yaml_scalar_style_t
-		   (bs-ref node 'data 'scalar 'style)))
-	   (raw (bytestructure-ref node 'data 'scalar 'value))
-	   (val (ffi:pointer->string (ffi:make-pointer raw))))
-      val))
+    (let* ((style (cdata*-ref node 'data 'scalar 'style))
+           (ptr-value (cdata*-ref node 'data 'scalar 'value))
+           (value (ffi:pointer->string ptr-value)))
+      value))
 
   (define (cnvt-sequence-node node)
-    (let* ((style (wrap-yaml_sequence_style_t
-		   (bs-ref node 'data 'sequence 'style)))
-	   (start (bs-ref node 'data 'sequence 'items 'start))
-	   (end (bs-ref node 'data 'sequence 'items 'end))
-	   (top (bs-ref node 'data 'sequence 'items 'top))
-	   (item-size (bytestructure-descriptor-size yaml_node_item_t-desc)))
+    (let* ((style (cdata*-ref node 'data 'scalar 'style))
+           (start (ptr-addr (cdata*-ref node 'data 'sequence 'items 'start)))
+           (end (ptr-addr (cdata*-ref node 'data 'sequence 'items 'end)))
+           (top (ptr-addr (cdata*-ref node 'data 'sequence 'items 'top)))
+           (item-size (ctype-size yaml_node_item_t)))
       (let loop ((sequence '()) (addr (- top item-size)))
-	(if (>= addr start)
-	    (let* ((item (bytestructure int*-desc addr))
-		   (indx (1- (bs-ref item '*)))
-		   (nd (bs-ref stack indx)))
-	      (loop (cons (cnvt-node nd) sequence) (- addr item-size)))
-	    (list->vector sequence)))))
+        (if (>= addr start)
+            (let* ((item (make-cdata (cpointer 'int) addr))
+                   (indx (1- (cdata-ref item '*)))
+                   (nd (cdata&-sel stack indx)))
+              (loop (cons (cnvt-node nd) sequence) (- addr item-size)))
+            (list->vector sequence)))))
 
   (define (cnvt-mapping-node node)
-    (let* ((style (wrap-yaml_mapping_style_t
-		   (bs-ref node 'data 'mapping 'style)))
-	   (start (bs-ref node 'data 'mapping 'pairs 'start))
-	   (end (bs-ref node 'data 'mapping 'pairs 'end))
-	   (top (bs-ref node 'data 'mapping 'pairs 'top))
-	   (pair-size (bytestructure-descriptor-size yaml_node_pair_t-desc)))
+    (let* ((style (cdata*-ref node 'data 'scalar 'style))
+           (start (ptr-addr (cdata*-ref node 'data 'mapping 'pairs 'start)))
+           (end (ptr-addr (cdata*-ref node 'data 'mapping 'pairs 'end)))
+           (top (ptr-addr (cdata*-ref node 'data 'mapping 'pairs 'top)))
+           (pair-size (ctype-size yaml_node_pair_t)))
       (let loop ((mapping '()) (addr (- top pair-size)))
-	(if (>= addr start)
-	    (let* ((pair (bytestructure yaml_node_pair_t*-desc addr))
-		   (key-ix (1- (bs-ref pair '* 'key)))
-		   (key-nd (bs-ref stack key-ix))
-		   (val-ix (1- (bs-ref pair '* 'value)))
-		   (val-nd (bs-ref stack val-ix)))
-	      (loop (acons (cnvt-node key-nd) (cnvt-node val-nd) mapping)
-		    (- addr pair-size)))
-	    mapping))))
+        (if (>= addr start)
+            (let* ((pair (make-cdata yaml_node_pair_t* addr))
+                   (key-ix (1- (cdata*-ref pair 'key)))
+                   (key-nd (cdata&-sel stack key-ix))
+                   (val-ix (1- (cdata*-ref pair 'value)))
+                   (val-nd (cdata&-sel stack val-ix)))
+              (loop (acons (cnvt-node key-nd) (cnvt-node val-nd) mapping)
+                    (- addr pair-size)))
+            mapping))))
 
   (define (cnvt-node node)
-    (let ((type (wrap-yaml_node_type_t (bs-ref node 'type))))
+    (let ((type (cdata-ref node '* 'type)))
       (add-node-tag
        (case type
-	 ((YAML_SCALAR_NODE) (cnvt-scalar-node node))
-	 ((YAML_SEQUENCE_NODE) (cnvt-sequence-node node))
-	 ((YAML_MAPPING_NODE) (cnvt-mapping-node node))
-	 (else (error "missed type" type)))
-       (ffi:pointer->string (ffi:make-pointer (bs-ref node 'tag))))))
+         ((YAML_SCALAR_NODE) (cnvt-scalar-node node))
+         ((YAML_SEQUENCE_NODE) (cnvt-sequence-node node))
+         ((YAML_MAPPING_NODE) (cnvt-mapping-node node))
+         (else (error "missed type" type)))
+       (ffi:pointer->string (cdata*-ref node 'tag)))))
 
   (cnvt-node root))
 
 (define (read-yaml-file filename)
-  (let* ((parser (make-yaml_parser_t))
-	 (&parser (pointer-to parser))
-	 (document (make-yaml_document_t))
-	 (&document (pointer-to document))
-	 (file (if (access? filename R_OK)
-		   (fopen filename "r")
-		   (file-not-found filename))))
+  (let* ((parser (make-cdata yaml_parser_t))
+         (&parser (cdata& parser))
+         (document (make-cdata yaml_document_t))
+         (&document (cdata& document))
+         (file (if (access? filename R_OK)
+                   (fopen filename "r")
+                   (file-not-found filename))))
 
     (yaml_parser_initialize &parser)
     (yaml_parser_set_input_file &parser file)
     (yaml_parser_load &parser &document)
     
-    (let* ((start (fh-object-ref document 'nodes 'start))
-	   (stack (bytestructure yaml_node_t*-desc start))
-	   (root (fh-object-val (yaml_document_get_root_node &document)))
-	   (yaml (cnvt-tree root stack)))
+    (let* ((start (cdata-sel document 'nodes 'start))
+           (stack start)
+           (root (yaml_document_get_root_node &document))
+           (yaml (cnvt-tree root stack)))
       (yaml_document_delete &document)
       (yaml_parser_delete &parser)
       (fclose file)
